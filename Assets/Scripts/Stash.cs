4085832a6728c219a9ca76003efcf468b8d1a75c
Slavathehiller <59045539+Slavathehiller@users.noreply.github.com>
2021-11-29 18:47:23 +0300

    Закрашивание панели ресурсов

--- Assets/Scripts/Stash.cs
+++ Assets/Scripts/Stash.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Stash
 {
-    public static int CoinsCount = 100;
+    public static int CoinsCount;
     public static int[] ResourceCount = new int[7];
     public static int[] ResourcePrices = new int[7] { 1, 3, 5, 10, 15, 30, 60};
 }
